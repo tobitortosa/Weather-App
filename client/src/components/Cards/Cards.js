82f935f46db4dbf2abe74f6378eadee583e7fa95
Tobi Tortosa <tobiastortosa17@gmail.com>
2022-10-13 21:31:30 -0300

    finish

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -4,22 +4,26 @@ import s from "./Cards.module.css";
 
 export default function Cards({ cities, onClose }) {
   return (
-    <div className={s.container}>
-      {cities &&
-        cities.map((c) => (
-          <Card key={c.id}
-            max={c.max}
-            min={c.min}
-            name={c.name}
-            img={c.img}
-            onClose={() => onClose(c.id)}
-            id={c.id}
-            wind={c.wind}
-            clouds={c.clouds}
-            latitud={c.latitud}
-            longitud={c.longitud}
-          />
-        ))}
+    <div className={s.mainContainer}>
+      <h1>Search Your City!</h1>
+      <div className={s.container} >
+        {cities &&
+          cities.map((c) => (
+            <Card
+              key={c.id}
+              max={c.max}
+              min={c.min}
+              name={c.name}
+              img={c.img}
+              onClose={() => onClose(c.id)}
+              id={c.id}
+              wind={c.wind}
+              clouds={c.clouds}
+              latitud={c.latitud}
+              longitud={c.longitud}
+            />
+          ))}
+      </div>
     </div>
   );
 }
